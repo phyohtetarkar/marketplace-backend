cbe92592002e106e32c0b2ea38a4384e54afb673
MinnKhantKyaww <minkhant199701@gmail.com>
2023-02-10 11:19:59 +0630

    Add subscription controller and dto

---
+++ shoppingcenter-application/src/main/java/com/shoppingcenter/app/controller/subscription/dto/SubscriptionPlanDTO.java
@@ -0,0 +1,32 @@
+package com.shoppingcenter.app.controller.subscription.dto;
+
+import java.lang.reflect.Type;
+
+import org.modelmapper.TypeToken;
+
+import com.shoppingcenter.service.PageData;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SubscriptionPlanDTO {
+
+    private long id;
+
+    private String title;
+
+    private int duration;
+
+    private boolean promoUsable;
+
+    private double price;
+
+    private long createdAt;
+
+    public static Type pageType() {
+        return new TypeToken<PageData<SubscriptionPlanDTO>>() {
+        }.getType();
+    }
+}
