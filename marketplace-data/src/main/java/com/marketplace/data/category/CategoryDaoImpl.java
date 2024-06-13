413e5ef914da5512aa4b66b274e76e15de04e0da
Phyo Htet Arkar <phyohtetarkar@gmail.com>
2024-06-14 00:00:51 +0630

    fixed category name cannot update

--- marketplace-data/src/main/java/com/marketplace/data/category/CategoryDaoImpl.java
+++ marketplace-data/src/main/java/com/marketplace/data/category/CategoryDaoImpl.java
@@ -19,130 +19,128 @@ import com.marketplace.domain.common.PageQuery;
 @Repository
 public class CategoryDaoImpl implements CategoryDao {
 
-    @Autowired
-    private CategoryRepo repo;
-    
-    @Autowired
-    private CategoryNameRepo nameRepo;
+	@Autowired
+	private CategoryRepo repo;
 
-    @Override
-    public Category save(CategoryInput values) {
-        var entity = repo.findById(values.getId()).orElseGet(CategoryEntity::new);
-        entity.setFeatured(values.isFeatured());
-        entity.setSlug(values.getSlug());
-        
-        if (values.getCategoryId() != null) {
-            entity.setCategory(repo.getReferenceById(values.getCategoryId()));
-        }
-        
-        var result = repo.save(entity);  
-        
-        if (values.getNames() != null) {
-        	var names = values.getNames().stream().map(n -> {
-        		var en = new CategoryNameEntity();
-        		en.getId().setLang(n.getLang());
-        		en.setCategory(result);
-        		en.setName(n.getName());
-        		return en;
-        	}).toList();
-        	nameRepo.saveAll(names);
-        }
-        
-        return CategoryMapper.toDomain(result);
-    }
+	@Autowired
+	private CategoryNameRepo nameRepo;
 
-    @Override
-    public void saveLftRgt(List<Category> list) {
-    	for (var c : list) {
-    		var entity = repo.findById(c.getId()).orElse(null);
-            
-            if (entity == null) {
-            	continue;
-            }
-            
-            if (entity.getLft() == c.getLft() && entity.getRgt() == c.getRgt()) {
-            	continue;
-            }
-            
-            repo.updateLftRgt(entity.getId(), c.getLft(), c.getRgt());
-    	}
-    }
-    
-    @Override
-    public void updateImage(int id, String image) {
-    	repo.updateImage(id, image);
-    }
+	@Override
+	public Category save(CategoryInput values) {
+		var entity = repo.findById(values.getId()).orElseGet(CategoryEntity::new);
+		entity.setFeatured(values.isFeatured());
+		entity.setSlug(values.getSlug());
 
-    @Override
-    public void delete(int id) {
-    	nameRepo.deleteByCategoryId(id);
-        repo.deleteById(id);
-        
-    }
+		if (values.getCategoryId() != null) {
+			entity.setCategory(repo.getReferenceById(values.getCategoryId()));
+		}
 
-    @Override
-    public boolean existsById(int id) {
-        return repo.existsById(id);
-    }
+		var result = repo.save(entity);
 
-    @Override
-    public boolean existsByCategory(int id) {
-        return repo.existsByCategory_Id(id);
-    }
+		if (values.getNames() != null) {
+			values.getNames().forEach(n -> {
+				var id = new CategoryNameEntity.ID(result.getId(), n.getLang());
+				var en = nameRepo.findById(id).orElse(null);
+				if (en == null) {
+					en = new CategoryNameEntity();
+					en.getId().setLang(n.getLang());
+					en.setCategory(result);
+				}
+				en.setName(n.getName());
+				nameRepo.save(en);
+			});
+		}
 
-    @Override
-    public boolean existsBySlug(String slug) {
-        return repo.existsBySlug(slug);
-    }
-    
-    @Override
-    public boolean existsByIdNotAndSlug(int id, String slug) {
-    	return repo.existsByIdNotAndSlug(id, slug);
-    }
+		return CategoryMapper.toDomain(result);
+	}
 
-    @Override
-    public String getCategoryImage(int id) {
-        return repo.getCategoryById(id, CategoryImageView.class).map(CategoryImageView::getImage).orElse(null);
-    }
+	@Override
+	public void saveLftRgt(List<Category> list) {
+		for (var c : list) {
+			var entity = repo.findById(c.getId()).orElse(null);
 
-    @Override
-    public Category findById(int id) {
-        return repo.findById(id).map(CategoryMapper::toDomain).orElse(null);
-    }
+			if (entity == null) {
+				continue;
+			}
 
-    @Override
-    public Category findBySlug(String slug) {
-        return repo.findBySlug(slug).map(CategoryMapper::toDomain).orElse(null);
-    }
+			if (entity.getLft() == c.getLft() && entity.getRgt() == c.getRgt()) {
+				continue;
+			}
 
-    @Override
-    public List<Category> findRootCategories() {
-        return repo.findByCategoryNull().stream()
-                .map(CategoryMapper::toDomainCompat)
-                .collect(Collectors.toList());
-    }
+			repo.updateLftRgt(entity.getId(), c.getLft(), c.getRgt());
+		}
+	}
 
-    @Override
-    public List<Category> findAll(boolean withNames) {
-        return repo.findAll().stream()
-                .map(e -> CategoryMapper.toDomain(e, withNames))
-                .collect(Collectors.toList());
-    }
-    
-    @Override
-    public List<Category> findByCategory(int categoryId) {
-    	return repo.findByCategoryId(categoryId).stream()
-                .map(CategoryMapper::toDomainCompat)
-                .collect(Collectors.toList());
-    }
+	@Override
+	public void updateImage(int id, String image) {
+		repo.updateImage(id, image);
+	}
 
-    @Override
-    public PageData<Category> findAll(PageQuery pageQuery) {
-        var request = PageQueryMapper.fromPageQuery(pageQuery);
+	@Override
+	public void delete(int id) {
+		nameRepo.deleteByCategoryId(id);
+		repo.deleteById(id);
 
-        Page<CategoryEntity> pageResult = repo.findAll(request);
+	}
 
-        return PageDataMapper.map(pageResult, CategoryMapper::toDomainCompat);
-    }
+	@Override
+	public boolean existsById(int id) {
+		return repo.existsById(id);
+	}
+
+	@Override
+	public boolean existsByCategory(int id) {
+		return repo.existsByCategory_Id(id);
+	}
+
+	@Override
+	public boolean existsBySlug(String slug) {
+		return repo.existsBySlug(slug);
+	}
+
+	@Override
+	public boolean existsByIdNotAndSlug(int id, String slug) {
+		return repo.existsByIdNotAndSlug(id, slug);
+	}
+
+	@Override
+	public String getCategoryImage(int id) {
+		return repo.getCategoryById(id, CategoryImageView.class).map(CategoryImageView::getImage).orElse(null);
+	}
+
+	@Override
+	public Category findById(int id) {
+		return repo.findById(id).map(CategoryMapper::toDomain).orElse(null);
+	}
+
+	@Override
+	public Category findBySlug(String slug) {
+		return repo.findBySlug(slug).map(CategoryMapper::toDomain).orElse(null);
+	}
+
+	@Override
+	public List<Category> findRootCategories() {
+		return repo.findByCategoryNull().stream().map(CategoryMapper::toDomainCompat).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<Category> findAll(boolean withNames) {
+		return repo.findAll().stream().map(e -> CategoryMapper.toDomain(e, withNames)).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<Category> findByCategory(int categoryId) {
+		return repo.findByCategoryId(categoryId).stream().map(CategoryMapper::toDomainCompat)
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public PageData<Category> findAll(PageQuery pageQuery) {
+		var request = PageQueryMapper.fromPageQuery(pageQuery);
+
+		Page<CategoryEntity> pageResult = repo.findAll(request);
+
+		return PageDataMapper.map(pageResult, CategoryMapper::toDomainCompat);
+	}
 
 }
